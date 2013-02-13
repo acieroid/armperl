35723c32c581cd05fe9ca5b4e425c4d75cb21460
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-02-13 08:57:13 +0100

    Improve lexer

--- lexer.ml
+++ lexer.ml
@@ -42,6 +42,7 @@ type token =
   | STRING_LOWER_EQUALS
   | NOT2
   | SPACE
+  | END
 
 let implode l =
   let s = String.create (List.length l) in
@@ -138,3 +139,59 @@ let lexer stream =
     | [< 'c when c == '\'' >] -> STRING (lex_string stream '\'' [])
     | [< ''$' >] -> VAR (lex_identifier stream [])
     | [< 'c >] -> IDENTIFIER (lex_identifier stream [c]) (* TODO: change to a-zA-Z or something like that *)
+    | [< >] -> END
+
+let print_token = function
+  | VAR s -> print_string "VAR("; print_string s; print_string ")"
+  | INTEGER n -> print_string "INTEGER("; print_int n; print_string ")"
+  | STRING s -> print_string "STRING("; print_string s; print_string ")"
+  | IDENTIFIER s -> print_string "IDENTIFIER("; print_string s; print_string ")"
+  | SUB -> print_string "SUB"
+  | RETURN -> print_string "RETURN"
+  | CALL_MARK -> print_string "CALL_MARK"
+  | LBRACE -> print_string "LBRACE"
+  | RBRACE -> print_string "RBRACE"
+  | LPAR -> print_string "LPAR"
+  | RPAR -> print_string "RPAR"
+  | SEMICOLON -> print_string "SEMICOLON"
+  | COMMA -> print_string "COMMA"
+  | IF -> print_string "IF"
+  | UNLESS -> print_string "UNLESS"
+  | ELSE -> print_string "ELSE"
+  | ELSEIF -> print_string "ELSEIF"
+  | NOT -> print_string "NOT"
+  | PLUS -> print_string "PLUS"
+  | MINUS -> print_string "MINUS"
+  | TIMES -> print_string "TIMES"
+  | DIVIDE -> print_string "DIVIDE"
+  | ASSIGN -> print_string "ASSIGN"
+  | CONCAT -> print_string "CONCAT"
+  | LAZY_OR -> print_string "LAZY_OR"
+  | LAZY_AND -> print_string "LAZY_AND"
+  | EQUALS -> print_string "EQUALS"
+  | DIFFERENT -> print_string "DIFFERENT"
+  | GREATER -> print_string "GREATER"
+  | LOWER -> print_string "LOWER"
+  | GREATER_EQUALS -> print_string "GREATER_EQUALS"
+  | LOWER_EQUALS -> print_string "LOWER_EQUALS"
+  | STRING_EQUALS -> print_string "STRING_EQUALS"
+  | STRING_DIFFERENT -> print_string "STRING_DIFFERENT"
+  | STRING_GREATER -> print_string "STRING_GREATER"
+  | STRING_LOWER -> print_string "STRING_LOWER"
+  | STRING_GREATER_EQUALS -> print_string "STRING_GREATER_EQUALS"
+  | STRING_LOWER_EQUALS -> print_string "STRING_LOWER_EQUALS"
+  | NOT2 -> print_string "NOT2"
+  | SPACE -> print_string "SPACE"
+  | END -> print_string "END"
+
+let rec loop s =
+  match (lexer s) with
+    | END -> ()
+    | SPACE -> loop s
+    | x -> print_token x; print_string " "; loop s
+
+let () =
+  while true do
+    loop (Stream.of_string (input_line stdin))
+ done
+
