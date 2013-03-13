7c3b8d1e290d14119d4b6445b93d37e9bbe1f95f
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-03-13 17:37:58 +0100

    Generate function calls

--- codegen.ml
+++ codegen.ml
@@ -113,7 +113,7 @@ let output_header channel =
 "
 
 (**
-  Generate the assembly code to load a value in the register r3.
+  Generate the assembly code to load a value in the register r4.
 
   Integers are left shifted by one bit, and added to 1, such that the
   least significant bit of an integer is always 1 (thus, integers are
@@ -129,17 +129,17 @@ let output_header channel =
 *)
 let gen_value state = function
   | Integer x -> state_add state ("
-    mov r3, #" ^ (string_of_int (box_int x)))
+    mov r4, #" ^ (string_of_int (box_int x)))
   | True -> state_add state ("
-    mov r3, #" ^ (string_of_int (box_int 1)))
+    mov r4, #" ^ (string_of_int (box_int 1)))
   | False -> state_add state ("
-    mov r3, #" ^ (string_of_int (box_int 0)))
+    mov r4, #" ^ (string_of_int (box_int 0)))
   | String str ->
       let addr = state_string_addr state str in
       state_add state ("
-    ldr r3, .L" ^ addr)
+    ldr r4, .L" ^ addr)
   | Undef -> state_add state "
-    mov r3, #2"
+    mov r4, #2"
   | Float _ -> failwith "Floats are unsupported"
 
 (* Load a local variable *)
@@ -150,7 +150,7 @@ let gen_local state v =
 let gen_global state v =
   let addr = state_global_addr state v in
   state_add state ("
-    ldr r3, " ^ addr)
+    ldr r4, " ^ addr)
 
 (* Generate multiple instructions and return the number of bytes
   needed on the stack for those instructions *)
@@ -175,7 +175,10 @@ and gen_instr state = function
   | Or (e1, e2) -> failwith "Not implemented"
   | And (e1, e2) -> failwith "Not implemented"
   | UnOp (op, e) -> failwith "Not implemented"
-  | Funcall (fname, args) -> failwith "Not implemented"
+  | Funcall (fname, args) ->
+      (* TODO: check that the number of arguments is correct *)
+      (* TODO: merge the strings for print ? *)
+      gen_funcall state fname args
   | Cond (cond, consequent, alternative) -> failwith "Not implemented"
   | CondEnd -> failwith "Not implemented"
   | Return x -> failwith "Not implemented"
@@ -185,22 +188,49 @@ and gen_instr state = function
 and gen_assign_local state var value =
   let addr = state_get_arg_addr state var
   and stack_needed = gen_instr state value in
-  (* copy the value from r3 to the argument *)
+  (* copy the value from r4 to the argument *)
   state_add state ("
-    str r3, " ^ addr);
+    str r4, " ^ addr);
   stack_needed
 
 (* Assign a value to a global variable *)
 and gen_assign_global state var value =
   let addr = state_global_addr state var
   and stack_needed = gen_instr state value in
-  (* Load global into r2 and store the value (which is in r3)
+  (* Load global into r5 and store the value (which is in r4)
      into the global *)
   state_add state ("
-    ldr r2, " ^ addr ^ "
-    str r3, [r2, #0]");
+    ldr r5, " ^ addr ^ "
+    str r4, [r5, #0]");
   stack_needed;
 
+(* Generate a function call *)
+and gen_funcall state fname args =
+  let stack_needed =
+    if List.length args > 4 then
+      ((List.length args)-4)*4
+    else
+      0
+  in
+  (* Generate the arguments *)
+  let stack_needed_l = List.mapi (fun i arg ->
+    (* Generate this argument *)
+    let stack_needed = gen_instr state arg in
+    (* Put it in the correct register or push it on the stack *)
+    if i < 4 then
+      state_add state ("
+    mov r" ^ (string_of_int i) ^ ", r4")
+    else
+      state_add state ("
+    str r4, [sp, #" ^ (string_of_int ((i-4)*4)) ^ "]");
+    stack_needed) args in
+  (* Call the function *)
+  state_add state ("
+    bl " ^ fname);
+  (* Return the stack size needed *)
+  List.fold_left max 0 (stack_needed::stack_needed_l)
+
+(* Generate a function definition *)
 and gen_fun state = function
   | Fundef (fname, args, body) ->
       let stack_needed =
@@ -227,6 +257,8 @@ and gen_fun state = function
     ldmfd   sp!, {fp, pc}
     .size " ^ fname ^ ", .-" ^ fname)
   | _ -> failwith "Not a function definition"
+
+
 
 let gen channel (funs, instrs) =
   let state = create_state () in
