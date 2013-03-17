eeb550553a97e761220df0f05ee34d2f77a82069
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-03-17 12:01:00 +0100

    Finishing report

--- eval.mli
+++ eval.mli
@@ -1,3 +1,5 @@
+(** Evaluator of Perl expressions *)
+
 (** Evaluate a sequence of Perl expressions *)
 val eval_sequence : Eval_symtable.t -> bool -> Expression.expr list -> Expression.value * Eval_symtable.t
 
